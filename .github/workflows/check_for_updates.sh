369767631a8b2e225484d3fe178719efbfeaf538
John Landis <jlandis@bcorporation.net>
2022-11-23 09:09:10 -0500

    Github action - New upstream heroku buildpack changes - 138bdbad6541a5a7fe20146666b9d0d2b27d3f52

---
+++ .github/workflows/check_for_updates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+UPSTREAM="https://github.com/heroku/heroku-buildpack-go.git"
+REMOTE=$(git remote -v)
+if [[ "${REMOTE}" != *"${UPSTREAM}"* ]]; then
+    git remote add upstream "${UPSTREAM}"
+fi
+
+git fetch upstream
+git pull origin main:upstream 1> /dev/null
+
+if [[ "$(git status --porcelain)" ]]; then
+    LATEST_COMMIT=$(git rev-parse @{u})
+
+    git add .
+    git commit -m "Github action - New upstream heroku buildpack changes - ${LATEST_COMMIT}"
+    gh pr create --title "New upstream heroku buildpack changes - ${LATEST_COMMIT}" --body "Most recent upstream changes"
+else
+    echo "repo is up to date with remote"
+fi
